a034572222be72cfa203fece761fa791a30c15e1
gus <gustavares.dev@gmail.com>
2023-06-01 18:43:29 -0300

    main file

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"net/http"
+	"tasks/api"
+	"tasks/config"
+	"tasks/datastore"
+)
 
+func main() {
+	c := config.New()
+
+	// db connection
+	db := datastore.New(c)
+
+	// setting up routes
+	h := api.NewHandlers(db)
+	r := h.RegisterRoutes()
+
+	port := flag.String("port", ":3001", "to-do")
+	http.ListenAndServe(*port, r)
 }
